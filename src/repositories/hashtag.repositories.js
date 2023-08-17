01bf3fd38b9820b08338b289141a08e20d4f145f
GDMeira <gabrieldega970@gmail.com>
2023-08-17 17:57:53 -0300

    feat: criado o endpoint de get trends que retorna as 10 hashtags com mais usos.

--- src/repositories/hashtag.repositories.js
+++ src/repositories/hashtag.repositories.js
@@ -2,9 +2,29 @@ import { db } from "../database/database.js";
 
 export function readPostsWithHashtag(hashtag) {
     return db.query(`
-        SELECT * 
-        FROM posts
-        JOIN posts_hashtags ON posts_hashtags."postId" = posts.id
+        SELECT 
+            p.id, p.url, p."postDescription", p."linkTitle", p."linkDescription", p."linkImage", 
+            (
+                SELECT json_build_object("pictureUrl",users."pictureUrl", "userName", users."userName")
+                FROM users WHERE users.id = p."userId"
+            ) AS writer,
+            (
+                SELECT users."userName"
+                FROM likes
+                JOIN users ON users.id = likes."userId"
+                WHERE likes."postId" = p.id
+            ) AS likers
+        FROM posts AS p
+        JOIN posts_hashtags ON posts_hashtags."postId" = p.id
         WHERE posts_hashtags."hashtagName" = $1;
+    `, [hashtag]);
+}
+
+export function readHashtags(hashtag) {
+    return db.query(`
+        SELECT hashtags.name
+        FROM hashtags
+        ORDER BY hashtags.count DESC
+        LIMIT 10;
     `, [hashtag]);
 }
