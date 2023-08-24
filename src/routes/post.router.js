ff7bcc75ffc3ca43b78ff999f49b54d159f08ae0
Gabriel <GABRIELDEGA970@GMAIL.COM>
2023-08-24 14:17:50 -0300

    Update post.router.js

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -10,7 +10,6 @@ const postsRouter = Router()
 postsRouter.get("/home", tokenValidation, getposts);
 postsRouter.post("/home", tokenValidation, validadeSchema(postSchema), postLink)
 postsRouter.get("/posts/:id", tokenValidation, getPostById);
-//postsRouter.delete("/home/:id", tokenValidation, deletePost);
 postsRouter.get("/user/:id", tokenValidation, getPostByUser);
 postsRouter.post("/posts/:postId/like", tokenValidation, postLike);
 postsRouter.delete("/posts/:postId/dislike", tokenValidation, deleteLike);
